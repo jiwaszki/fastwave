fa5236a61fb3dd1b71d59eb7bc568a5a9952f313
jiwaszki <jiwaszkiewicz6@gmail.com>
2023-12-30 00:45:01 +0100

    Add convert_data helper and remove conversion from read function

--- src/fastwave/__init__.py
+++ src/fastwave/__init__.py
@@ -14,3 +14,4 @@ from .impl import AudioData
 from .impl import ReadMode
 from .impl import info
 from .impl import read
+from .impl import convert_data
--- src/fastwave/impl.py
+++ src/fastwave/impl.py
@@ -1,3 +1,6 @@
+from functools import singledispatch
+from typing import Union
+
 import numpy as np
 
 from ._fastwave import AudioInfo
@@ -7,17 +10,49 @@ from ._fastwave import info
 from ._fastwave import read as _read
 
 
+# Helpers to work with data convertions
+# TODO: how to include it into "read" function and keep RO on "data"?
+def convert_data(audio: Union[AudioData, np.ndarray], *, mono: bool = False, dtype = None):
+    raise NotImplemented("Unknown dispatch has been used!")
+
+
+@convert_data.register(AudioData)
+def _(audio: AudioData, *, mono: bool = False, dtype = None) -> np.ndarray:
+    return convert_data(audio.data, mono=mono, dtype=dtype)
+
+
+@convert_data.register(np.ndarray)
+def _(audio: np.ndarray, *, mono: bool = False, dtype = None) -> np.ndarray:
+
+    _data = audio.data
+
+    if dtype is not None:
+        # TODO: check if dtype is correct
+        _data = _data.astype(dtype)
+        if dtype == np.float32:
+            _data = _data / 32767.
+        else:
+            raise NotImplemented(f"Conversion for {dtype} is not supported!")
+    # Currently only two channels are supported:
+    if mono and _data.shape[0] == 2:
+        # Originally samples first, change to channels first
+        _data = _data.T
+        _data = (_data[0] + _data[1]) / 2
+
+    return _data
+
+
 def read(
     file_path,
-    mono: bool = True,
-    dtype = None,
+    *,
     mode: ReadMode = ReadMode.DEFAULT,
-    *, 
     cache_size: int = 131072,  # in bytes
     num_threads: int = 8,  # TODO: make it more generic
 ) -> AudioData:
     # TODO: add one extra mode of "PYTHON"/"NATIVE"
     # This will not release the GIL but give native wrapper
+    # Downside is that it needs some kind of heuristic first
+    # to determine when this method is better.
     # import wave
     # import numpy as np
     # w = wave.open("./test.wav", "rb")
@@ -26,23 +61,5 @@ def read(
     # w.rewind() ?
     # w.close()
     # return data (+ info? in some structure)
-
-    # return always 1D and reshape it and transpose accordingly?
-    # TODO: benchmark, this can work better as numpy can speed-up comp. with multiple threads
-    # data, info = _read(file_path, mode, num_threads)
-    
-    # # Originally samples first, change to channels first
-    # if data.shape[0] != 1:
-    #     data = data.T
-
-    # if dtype is not None:
-    #     # TODO: check if dtype is correct
-    #     data = data.astype(dtype)
-    #     if dtype == np.float32:
-    #         data = data / 32767.
-
-    # # Currently only two channels are supported:
-    # if mono and data.shape[0] == 2:
-    #     data = (data[0] + data[1]) / 2
 
     return _read(file_path, mode, cache_size, num_threads)
