b0cb83af03b30f7f92db8aa7ea70dd2e5c61caac
jiwaszki <jiwaszkiewicz6@gmail.com>
2023-12-30 03:55:09 +0100

    Add info tests

--- tests/test_info.py
+++ tests/test_info.py
@@ -15,13 +15,7 @@ FILES = (
 
 
 def ref_read(file_path):
-    info = {}
-    _info = torchaudio.info(file_path)
-    info["duration"] = _info["samples"] / _info["sampling_rate"]
-    info["num_samples"] = _info.num_frames
-    info["num_channels"] = _info.num_channels
-    info["sample_rate"] = _info.sample_rate
-    return info
+    return torchaudio.info(file_path)
 
 
 @pytest.mark.parametrize(
@@ -31,11 +25,11 @@ def ref_read(file_path):
 def test_info_direct(file_name):
     ref_info = ref_read(file_name)
     audio_info = f.info(file_name)
-    assert ref_info["duration"] == audio_info.duration
-    assert ref_info["num_samples"] == audio_info.num_samples
-    assert ref_info["num_channels"] == audio_info.num_channels
-    assert ref_info["sample_rate"] == audio_info.sample_rate
-    # assert ref_info["bit_depth"] == audio_info.bit_depth
+    assert ref_info.num_frames / ref_info.sample_rate == audio_info.duration
+    assert ref_info.num_frames == audio_info.num_samples
+    assert ref_info.num_channels == audio_info.num_channels
+    assert ref_info.sample_rate == audio_info.sample_rate
+    assert ref_info.bits_per_sample == audio_info.bit_depth
 
 
 @pytest.mark.parametrize(
@@ -53,9 +47,9 @@ def test_info_direct(file_name):
 )
 def test_info_read(file_name, mode):
     ref_info = ref_read(file_name)
-    audio_info = f.read(file_name, mode=mode)
-    assert ref_info["duration"] == audio_info.duration
-    assert ref_info["num_samples"] == audio_info.num_samples
-    assert ref_info["num_channels"] == audio_info.num_channels
-    assert ref_info["sample_rate"] == audio_info.sample_rate
-    # assert ref_info["bit_depth"] == audio_info.bit_depth
+    audio_info = f.read(file_name, mode=mode).info
+    assert ref_info.num_frames / ref_info.sample_rate == audio_info.duration
+    assert ref_info.num_frames == audio_info.num_samples
+    assert ref_info.num_channels == audio_info.num_channels
+    assert ref_info.sample_rate == audio_info.sample_rate
+    assert ref_info.bits_per_sample == audio_info.bit_depth
